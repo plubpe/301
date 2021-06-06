3ea0805e8ce5382402eea207b2907abc78b205f4
plubpe <78285973+plubpe@users.noreply.github.com>
2021-06-06 11:21:51 +0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,31 @@
+pins.set_pull(DigitalPin.P12, PinPullMode.PullUp)
+pins.set_pull(DigitalPin.P13, PinPullMode.PullUp)
+serial.write_line("asd")
+def on_forever():
+    l=pins.digital_read_pin(DigitalPin.P12)
+    r=pins.digital_read_pin(DigitalPin.P13)
+    serial.write_value("l", l)
+    serial.write_value("r", r)
+    if l==0 and r==0 :
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 1)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+    elif l==0 and r==1 :
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+        basic.pause(100)
+    elif l==1 and r==0 :
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 1)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+        basic.pause(100)
+    else:
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,33 @@
+pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+serial.writeLine("asd")
+basic.forever(function on_forever() {
+    let l = pins.digitalReadPin(DigitalPin.P12)
+    let r = pins.digitalReadPin(DigitalPin.P13)
+    serial.writeValue("l", l)
+    serial.writeValue("r", r)
+    if (l == 0 && r == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    } else if (l == 0 && r == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        basic.pause(100)
+    } else if (l == 1 && r == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        basic.pause(100)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    }
+    
+})
